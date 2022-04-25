8682c68d27485658d8e9d9da301be8fe6abafe50
Tejas Chheda <tejas4888@gmail.com>
2022-04-25 12:13:17 -0400

    Removed commented code - minor change

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,18 @@
 import click
-# from visualize_saliency.visualize import visualize
-# from generate_attacks.generate import generate
-# from evaluation.evaluate import evaluate
-# from search.greedy_search import greedy_search
+from visualize_saliency.visualize import visualize
+from generate_attacks.generate import generate
+from evaluation.evaluate import evaluate
+from search.greedy_search import greedy_search
 from plot_graphs.plotting import plotting
 
 @click.group()
 def main():
     pass
 
-# main.add_command(visualize)
-# main.add_command(generate)
-# main.add_command(evaluate)
-# main.add_command(greedy_search)
+main.add_command(visualize)
+main.add_command(generate)
+main.add_command(evaluate)
+main.add_command(greedy_search)
 main.add_command(plotting)
 
 if __name__ == '__main__':
