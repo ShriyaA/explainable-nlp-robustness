de83149108cbed65f15685231c1bc5e98c7037a5
Tejas Chheda <tejas4888@gmail.com>
2022-04-28 01:41:23 -0400

    Added command for sentence similarity - basic structure only

--- src/__main__.py
+++ src/__main__.py
@@ -4,6 +4,7 @@ from generate_attacks.generate import generate
 from evaluation.evaluate import evaluate
 from search.greedy_search import greedy_search
 from plot_graphs.plotting import plotting
+from similarity.sent_similarity import sent_similarity
 
 @click.group()
 def main():
@@ -14,6 +15,7 @@ main.add_command(generate)
 main.add_command(evaluate)
 main.add_command(greedy_search)
 main.add_command(plotting)
+main.add_command(sent_similarity)
 
 if __name__ == '__main__':
     main()
